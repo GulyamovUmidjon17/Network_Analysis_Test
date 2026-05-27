e9d1e108ad2be584078a4ebe5ac34572a6cc9cc9
GulyamovUmidjon17 <unidgulyamov@gmail.com>
2026-05-27 21:31:49 +0500

    Initial commit

--- ping_check.py
+++ ping_check.py
@@ -1,11 +1,32 @@
 import subprocess
+from datetime import datetime
 
-hosts = ["8.8.8.8", "google.com", "192.168.1.1"]
+#Список хостов для проверки
+HOSTS = [
+    "8.8.8.8",
+    "google.com",
+    "192.168.1.1"
+]
 
-for host in hosts:
-    result = subprocess.run(["ping", "-c", "1", host], capture_output=True)
-    
+def ping_host(host):
+    """
+    Проверка доступности хостов ICMP пакетом.
+    """
+    result = subprocess.run(
+        ["ping", "-c", "1", "-W", "2", host],
+        capture_output=True,
+        text=True
+    )
+
+    current_time = datetime.now().strftime("%H:%M:%S")
+
     if result.returncode == 0:
-        print(f"{host} is UP")
+        status = "UP"
     else:
-        print(f"{host} is DOWN")
+        status = "DOWN"
+    print(f"[{current_time}]  {host:<15}  STATUS: {status}")
+print("\n========== СОСТОЯНИЕ СЕТИ ==========\n")
+
+for host in HOSTS:
+    ping_host(host)
+print("\n=============== КОНЕЦ ================\n")
